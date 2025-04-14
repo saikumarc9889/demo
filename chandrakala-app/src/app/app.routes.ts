0d203ad02ff5e46aa50b6229869f72e5b7b566be
= <=>
2025-04-14 20:42:34 +0530

    c

--- chandrakala-app/src/app/app.routes.ts
+++ chandrakala-app/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        loadComponent:() => import('./components/main/main.component').then(m => m.MainComponent),
+    },
+    {path:'',redirectTo:'',pathMatch:'full'},
+    {path:'**',
+        loadComponent:() => import('./shared/page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent),
+    }
+];
---
+++ chandrakala-app/src/app/components/signup/signup.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cmp-signup',
+  imports: [],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.scss'
+})
+export class SignupComponent {
+
+}
